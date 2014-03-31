a83bd9465b8563dce06ed328d3ba42ec04fff12c
Bradley Smith <bradleydsmith@gmail.com>
2014-03-30 20:44:32 -0600

    add render to lib

---
+++ lib/markdown_server/renderer.ex
@@ -0,0 +1,21 @@
+defrecord MarkdownServer.RenderedDocument, body: nil, title: "untitled"
+
+defmodule MarkdownServer.Renderer do
+  def render_string(str) do
+    body = str |> Markdown.to_html 
+    MarkdownServer.RenderedDocument[body: body, title: title_for(body)]    
+  end
+
+  defp title_matcher do
+    %r/<h1>(?<title>.*)<\/h1>/g
+  end
+
+  defp title_for(body) do
+    case Regex.named_captures(title_matcher, body) do
+      [title: title] -> title
+      nil -> "untitled" 
+    end
+  end
+
+end
+
--- test/renderer_test.exs
+++ test/renderer_test.exs
@@ -1,12 +1,3 @@
-defrecord MarkdownServer.RenderedDocument, body: nil, title: "untitled"
-
-defmodule MarkdownServer.Renderer do
-  def render_string(str) do
-    body = str |> Markdown.to_html 
-    MarkdownServer.RenderedDocument[body: body]    
-  end
-end
-
 defmodule MarkdownServer.RendererTest do
   use ExUnit.Case
 
@@ -15,6 +6,12 @@ defmodule MarkdownServer.RendererTest do
     expected_body = "<p>this document has no title</p>\n"
     expected_title = "untitled"
     assert MarkdownServer.RenderedDocument[title: expected_title, body: expected_body] == rendered_document
+  end
+
+  test "extracts title from document" do
+    rendered_document = MarkdownServer.Renderer.render_string("# This is a title\n\n this document has a title")
+   expected_title = "This is a title"
+   assert expected_title == rendered_document.title 
   end
   
 end
